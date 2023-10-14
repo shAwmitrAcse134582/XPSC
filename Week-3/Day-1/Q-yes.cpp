ffdb0ef447b6dc5f95a17577a53aeecdf04d5922
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-10-14 21:39:00 +0600

    solved 3 problems

---
+++ Week-3/Day-1/Q-yes.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll  long long int
+#define pii pair<int,int>
+#define pb  push_back
+#define get_out return 0
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+const int N=1e5+7;
+int main()
+{
+fast;
+ll t;
+cin>>t;
+while(t--){
+       string s;
+  		cin>>s;
+  		string st="";
+  		
+  		for(ll i=0;i<20;i++)
+  		{
+  			st+="Yes";
+  		}
+  		if((st.find(s))!=-1)
+  		yes;
+  		else
+  		no;
+    
+}
+get_out;
+}
---
+++ Week-3/Day-1/p-spellcheck.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll  long long int
+#define pii pair<int,int>
+#define pb  push_back
+#define get_out return 0
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+const int N=1e5+7;
+int main()
+{
+fast;
+ll t;
+cin>>t;
+while(t--){
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    string s1="Timru";
+    sort(s.begin(),s.end());
+    // cout<<s<<endl;
+
+    if(s==s1){
+        yes;
+    }
+    else{
+        no;
+    }
+    
+}
+get_out;
+}
